6db126f4e01211edfa964da234c960b85b63bef1
rebeccabuie <rebeccabuie@gmail.com>
2017-01-10 14:30:47 -0700

    Added Portfolio Feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,54 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    if @portfolio_item.save
+      redirect_to @portfolio_item
+    else
+      render :new
+    end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    if @portfolio_item.update(portfolio_params)
+      redirect_to @portfolio_item
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    redirect_to portfolios_path
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
+
+
 end
+
+
+
+
