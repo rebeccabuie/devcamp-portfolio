b9a529df01f21359ed145b31c577c179bf34301f
rebeccabuie <rebeccabuie@gmail.com>
2017-01-23 18:24:45 -0700

    Saving files for the authorization branch and switching to view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
+  # def login_helper
+  #   if current_user.is_a?(GuestUser)
+  #     (link_to "Register", new_user_registration_path) +
+  #     "<br>".html_safe +
+  #     (link_to "Login", new_user_session_path)
+  #   else
+  #     link_to "logout", destroy_user_session_path, method: :delete
+  #   end
+  # end
+
   def copyright_generator
     BuieViewTool::Renderer.copyright 'Rebecca Buie', 'All Rights Reserved'
   end
 end
+ 
