de5deee46290537ef1503b3f7bf197f5ab075de4
rebeccabuie <rebeccabuie@gmail.com>
2017-01-31 14:57:24 -0700

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout."
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
@@ -59,16 +59,16 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Rebecca Buie's Portfolio", sticky: false)
+  end
+
 end
-
-
-
-
-
-
-
-
-
-
-
-
