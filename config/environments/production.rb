3420e746eeef0c3030c4f8a5823c293fd67b44e7
rebeccabuie <rebeccabuie@gmail.com>
2017-02-07 14:42:37 -0700

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "DevCampPortfolio_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "DevcampPortfolio_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://devcamp-portfolio.herokuapp.com', 'http://devcamp-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://devcamp-portfolio.herokuapp.com/cable"
 end
